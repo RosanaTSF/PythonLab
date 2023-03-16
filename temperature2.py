9e43330b12b0e58656fd1c96c42bf72222065748
Rosana <rosanatsf@gmail.com>
2023-03-16 12:51:46 -0300

    Experiments in Python

---
+++ temperature2.py
@@ -0,0 +1,12 @@
+temp = eval(input('Enter the current temperature:'))
+
+if temp >= 26:
+    print('Its hot!')
+    print('Drink plenty of water.')
+    print('Bye bye.')
+
+else:
+
+    print('Is cold!')
+    print('Bring a jacket!')
+    print('Bye bye.')
