02e0c343e4d62696df697d37cfb3d631d8f12826
Rosana <rosanatsf@gmail.com>
2023-04-18 21:28:28 -0300

    Experiments in Python

---
+++ algoritms2/poo/poo3.py
@@ -0,0 +1,15 @@
+import math
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor('pink')
+alex = turtle.Turtle()
+
+alex.goto(150, 160)
+alex.forward(100)
+alex.left(190)
+alex.forward(175)
+wn.exitonclick()
+
+wn = turtle.Screen()
+
---
+++ algoritms2/poo/poo5.py
@@ -0,0 +1,10 @@
+import random
+
+prob = random.random()
+print(prob)
+
+lanceDado = random.randrange(1, 7)
+print(lanceDado)
+
+resultado = prob * 5
+print(resultado)
